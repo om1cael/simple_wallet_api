1304a63f55028598641cb83da1614aa65a30e8c3
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-01 17:56:09 -0300

    feat: verify jwt token

--- src/main/java/com/om1cael/simple/wallet/utils/JwtUtil.java
+++ src/main/java/com/om1cael/simple/wallet/utils/JwtUtil.java
@@ -1,7 +1,10 @@
 package com.om1cael.simple.wallet.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +18,19 @@ public class JwtUtil {
         return JWT.create()
                 .withIssuer("simplewallet")
                 .sign(algorithm);
+    }
+
+    boolean verify(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(jwtToken);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("simplewallet")
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
     }
 }
