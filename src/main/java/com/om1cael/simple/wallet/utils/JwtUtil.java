3c91c2758b5a2597dbcaea01778afe390437433a
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-01 17:58:38 -0300

    feat: save email on jwt token

--- src/main/java/com/om1cael/simple/wallet/utils/JwtUtil.java
+++ src/main/java/com/om1cael/simple/wallet/utils/JwtUtil.java
@@ -3,7 +3,6 @@ package com.om1cael.simple.wallet.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,24 +12,21 @@ public class JwtUtil {
     @Value("{jwt.token}")
     private String jwtToken;
 
-    String create() {
+    String create(String email) {
         Algorithm algorithm = Algorithm.HMAC512(jwtToken);
         return JWT.create()
                 .withIssuer("simplewallet")
+                .withSubject(email)
                 .sign(algorithm);
     }
 
-    boolean verify(String token) {
-        try {
-            Algorithm algorithm = Algorithm.HMAC512(jwtToken);
-            JWTVerifier verifier = JWT.require(algorithm)
-                    .withIssuer("simplewallet")
-                    .build();
+    String getSubject(String token) {
+        Algorithm algorithm = Algorithm.HMAC512(jwtToken);
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("simplewallet")
+                .build();
 
-            DecodedJWT decodedJWT = verifier.verify(token);
-            return true;
-        } catch (JWTVerificationException e) {
-            return false;
-        }
+        DecodedJWT decodedJWT = verifier.verify(token);
+        return decodedJWT.getSubject();
     }
 }
