4c658630e3ca75acfd2427550071c6b8884e145f
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-01 17:29:12 -0300

    feat: set user model

---
+++ src/main/java/com/om1cael/simple/wallet/models/User.java
@@ -0,0 +1,59 @@
+package com.om1cael.simple.wallet.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 8)
+    private String fullName;
+
+    @Column(unique = true)
+    @NotNull
+    private String document;
+
+    @Column(unique = true)
+    @NotNull
+    private String email;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @NotNull
+    private BigDecimal balance;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return fullName;
+    }
+}
